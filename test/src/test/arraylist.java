139ebc6ad9b6926c2502f8fea7b94cdcacd97e0c
psdemo <psdemo@DESKTOP-EEAD1M1>
2020-05-18 00:43:41 +0530

    added arraylist program

---
+++ test/src/test/arraylist.java
@@ -0,0 +1,31 @@
+package test;
+
+import java.util.ArrayList;
+
+public class arraylist {
+
+	private static final boolean String = false;
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		ArrayList<String> l1 =  new ArrayList<String>();
+		l1.add("test1");
+		l1.add("test2");
+		l1.add("test3");
+		int totalelement =  l1.size();
+		System.out.println(totalelement);
+		//String s = l1.get(0);
+		
+			System.out.println(l1);
+			System.out.println(l1.get(0));
+		
+		
+	}
+
+	private static void foreach(Object object) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
