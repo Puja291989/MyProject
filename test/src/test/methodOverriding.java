07829f99e66d1610abfde105ea4e64d278931ae8
psdemo <psdemo@DESKTOP-EEAD1M1>
2020-05-18 00:07:34 +0530

    adding methodoverriding program

---
+++ test/src/test/methodOverriding.java
@@ -0,0 +1,43 @@
+ package test;
+
+ class parentmethod
+{
+	
+	public void m1()
+	{
+		System.out.print("parent" );
+	}
+	
+	
+}
+
+
+public class methodOverriding extends parentmethod {
+	
+	public void m1()
+	{
+		System.out.print("child " );
+	}
+	
+	public void m2()
+	{
+		this.m1(); // to call method of its own class
+		super.m1(); // to call method of parent class
+	}
+
+
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		methodOverriding mo =  new methodOverriding();
+		mo.m1();
+		mo.m2();
+				
+			parentmethod p1 = new 	methodOverriding();
+			p1.m1();
+			
+		;
+			
+	}
+
+}
